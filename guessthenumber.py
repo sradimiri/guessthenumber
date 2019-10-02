4dc87c231712a904bf96dcb6fb191b2643a4bfd0
svenradimiri <svenradimiri@gmail.com>
2019-10-02 21:42:09 +0200

    initial commit

---
+++ guessthenumber.py
@@ -0,0 +1,8 @@
+secret = "41"
+
+guess = input("Guess the number between 1 and 100: ")
+
+if guess == secret:
+    print("Congratulations! You guessed the secret number!")
+else:
+    print("Sorry, that is not the secret number!")
