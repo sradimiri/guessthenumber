f87ed3c83fbf96176eaccc633a849b3d133e9907
svenradimiri <svenradimiri@gmail.com>
2019-10-08 22:00:24 +0200

    topscore

--- guessthenumber.py
+++ guessthenumber.py
@@ -12,8 +12,9 @@ while True:
     attempts += 1
 
     if guess == secret:
-        with open("score.txt", "w") as score_file:
-            score_file.write(str(attempts))
+        if attempts < best_score:
+            with open("score.txt", "w") as score_file:
+                score_file.write(str(attempts))
 
         print("Congratulations! You guessed the secret number, it's " + str(secret) +"!")
         print("Attempts needed: " + str(attempts))
