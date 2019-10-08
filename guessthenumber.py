d8c29665485d79b95c6269c086d61939224226c1
svenradimiri <svenradimiri@gmail.com>
2019-10-08 15:04:00 +0200

    random

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,11 +1,13 @@
-secret = 41
+import random
+
+secret = random.randint(1, 100)
 
 
 while True:
     guess = int(input("Guess the number between 1 and 100: "))
 
     if guess == secret:
-        print("Congratulations! You guessed the secret number!")
+        print("Congratulations! You guessed the secret number, it's " + str(secret) +"!")
         break
     elif guess < secret:
         print("That is not correct. The secret number is bigger.")
